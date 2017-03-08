ec244cf092115ea427068fbf1cbf0057c60e62db
er368 <er368@exeter.ac.uk>
2017-03-08 02:03:38 +0000

    some GUI work

--- Echo/src/Echo.java
+++ Echo/src/Echo.java
@@ -239,9 +239,9 @@ public class Echo extends JFrame {
         /**
          * Method listens for speech then returns it in String format
          */
-        System.out.println("Listening...!");
+        label1.setText("Listening...");
         RecordSound.record();   //This needs to be replaced by an automatic process
-        System.out.println("Done listening...!");
+        label1.setText("Please wait.");
         SpeechToText.convert();
         
         final String token  = SpeechToText.renewAccessToken( KEY1 );
@@ -271,6 +271,7 @@ public class Echo extends JFrame {
     
     public void answer(String question){
     
+        label2.setText("Answering...");
         String response = Wolfram.solve(question);
         
         switchModeTo(ANSWERMODE);
@@ -292,6 +293,8 @@ public class Echo extends JFrame {
                 topButton.turnOff();
                 light.turnOff();
                 topLight.turnOff();
+                label1.setText("");
+                label2.setText("");
                 //microphone disable
                 playSound( turnOffSound);
                 currentMode = OFFMODE;
